57e03e21ccfaac841a61b7f0fbbeedf542c10114
Stephen Dickinson <stephencottontail@me.com>
2019-10-31 21:34:07 -0600

    Styles and animation for the header

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,27 @@
+const fs = require( 'fs' );
+const path = require( 'path' );
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy( {
         "_includes/assets/fonts": "assets/fonts",
         "_includes/assets/images": "assets/images"
+    } );
+
+    eleventyConfig.addFilter( 'useSVG', function( value ) {
+        let relativeFilePath = `./_includes/${value}`;
+
+        if ( '.svg' != path.extname( value ) ) {
+            throw new Error( 'This filter expects an SVG' );
+        }
+
+        let data = fs.readFileSync( relativeFilePath, function( err, contents ) {
+            if ( err ) {
+                throw new Error( err );
+            }
+
+            return contents;
+        } );
+
+        return data.toString( 'utf-8' );
     } );
 };
